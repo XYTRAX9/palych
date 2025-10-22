217a7d9400d693a9cc345d10bf8a78e040952a21
GoldArr <goldarr500@gmail.com>
2025-10-22 17:04:41 +0500

    - Hotfix

--- routers/dictionaries_router.py
+++ routers/dictionaries_router.py
@@ -8,8 +8,7 @@ from database.schemas import (
     LessonTypeResponse,
     SkillsKnowledgeResponse
 )
-from database.dependencies import get_techcard_db  # вместо from main import
-
+from database.dependencies import get_main_db
 
 # Создаём роутер для справочников
 router = APIRouter(
@@ -17,41 +16,43 @@ router = APIRouter(
     tags=["Справочники"]
 )
 
+
 # Получить список всех групп
 @router.get("/groups", response_model=list[GroupResponse])
-def get_groups(db: Session = Depends(get_techcard_db)):
-    """
-    Возвращает список всех групп из справочника.
-    """
+def get_groups(db: Session = Depends(get_main_db)):
     return db.query(Group).all()
+
 
 # Получить список всех предметов
 @router.get("/lessons", response_model=list[LessonResponse])
-def get_lessons(db: Session = Depends(get_techcard_db)):
+def get_lessons(db: Session = Depends(get_main_db)):
     """
     Возвращает список всех предметов из справочника.
     """
     return db.query(Lesson).all()
 
+
 # Получить список всех преподавателей
 @router.get("/teachers", response_model=list[TeacherResponse])
-def get_teachers(db: Session = Depends(get_techcard_db)):
+def get_teachers(db: Session = Depends(get_main_db)):
     """
     Возвращает список всех преподавателей из справочника.
     """
     return db.query(Teacher).all()
 
+
 # Получить список всех типов уроков
 @router.get("/lesson-types", response_model=list[LessonTypeResponse])
-def get_lesson_types(db: Session = Depends(get_techcard_db)):
+def get_lesson_types(db: Session = Depends(get_main_db)):
     """
     Возвращает список всех типов уроков из справочника.
     """
     return db.query(LessonType).all()
 
+
 # Получить список знаний и умений
 @router.get("/skills-knowledge", response_model=list[SkillsKnowledgeResponse])
-def get_skills_knowledge(db: Session = Depends(get_techcard_db)):
+def get_skills_knowledge(db: Session = Depends(get_main_db)):
     """
     Возвращает список знаний и умений из справочника.
     """
